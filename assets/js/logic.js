cbc9f9750719a0d781acc05b4d478e01306bd26f
Sandeep Bhatula <sandeepbvv11@users.noreply.github.com>
2024-02-16 23:44:19 -0800

    WebAPI quiz application

---
+++ assets/js/logic.js
@@ -0,0 +1,240 @@
+ 
+    var startBtn = document.getElementById("startBtn");
+    var time = 75;
+    var time_remaining = true;
+    var time_start= false;
+    var countdownTimer = document.getElementById("countdownTimer");
+    var homeContainer =  document.getElementById("homeContainer");
+    var quizContainer = document.getElementById("quizContainer");
+    var questionHeading = document.getElementById("questionHeading");
+    var answerChoiceA = document.getElementById("answerChoiceA");
+    var answerChoiceB = document.getElementById("answerChoiceB");
+    var answerChoiceC = document.getElementById("answerChoiceC");
+    var answerChoiceD = document.getElementById("answerChoiceD");
+    var correctAnswer = document.getElementById("correctAnswer");    
+    var high_scores= [];
+    var output="";
+    
+    var score = 0;
+    
+    let i = 0;
+
+
+var countdownTimerInterval = setInterval(setCountdownTimer, 1000);
+
+
+function setCountdownTimer() {
+        if (time_start)
+        time--;
+        if(time<= 0) {
+        end_quiz();
+        time = 0;    
+        
+        }
+        document.getElementById("timer").innerHTML = time;
+    }
+
+
+startBtn.addEventListener("click", function() {
+    quizContainer.style.display = "block";
+    homeContainer.style.display ="none";
+    countdownTimer.style.display= "block";
+    document.getElementById("score_keeper").style.display= "block";
+    document.getElementById("score").innerHTML = score;
+    setCountdownTimer();
+    setQuizQuestions();
+    time_start= true;
+});
+
+
+
+function setQuizQuestions() {
+        questionHeading.textContent = questionsArray[i].question;
+        answerChoiceA.textContent = questionsArray[i].answerChoice[0]; 
+        answerChoiceB.textContent = questionsArray[i].answerChoice[1]; 
+        answerChoiceC.textContent = questionsArray[i].answerChoice[2]; 
+        answerChoiceD.textContent = questionsArray[i].answerChoice[3]; 
+        };
+
+
+answerChoiceA.addEventListener('click', function(event) {
+        event.stopPropagation();
+        correctAnswer= questionsArray[i].correctAnswer;
+        
+        
+        
+        if (questionsArray[i].correctAnswer === correctAnswer) { 
+            
+            document.getElementById("AnswerResponse").innerHTML = "Correct! Nailed it!";
+            setTimeout(function() {
+            document.getElementById("AnswerResponse").innerHTML = "";
+                },
+                1000
+            );
+            
+            score++;    
+            
+            document.getElementById("score").innerHTML = score;
+        } else {
+            time_remaining -= 5;
+            
+            document.getElementById("AnswerResponse").innerHTML = "Incorrect! Better luck in the next one!";
+            setTimeout(function() {
+                document.getElementById("AnswerResponse").innerHTML = "";
+                    },
+                    1000
+                );
+        }
+        if (i >= questionsArray.length -1) {
+        end_quiz();
+        } else {
+            i++ 
+            setQuizQuestions();
+        };
+    });
+
+answerChoiceB.addEventListener('click', function(event) {
+    event.stopPropagation();
+    correctAnswer = questionsArray[i].correctAnswer;
+    
+        if (questionsArray[i].correctAnswer === correctAnswer) { 
+            document.getElementById("AnswerResponse").innerHTML = "Correct! Nailed it!";
+            setTimeout(function() {
+                document.getElementById("AnswerResponse").innerHTML = "";
+                    },
+                    1000
+                );
+            score++;
+            document.getElementById("score").innerHTML = score;
+        } else {
+            time_remaining -= 5;
+            document.getElementById("AnswerResponse").innerHTML = "Incorrect! Better luck in the next one!";
+            setTimeout(function() {
+                document.getElementById("AnswerResponse").innerHTML = "";
+                    },
+                    1000
+                );
+        }
+        if (i >= questionsArray.length -1) {
+        end_quiz();
+        } else {
+         i++ 
+        setQuizQuestions();
+        };
+    });
+
+answerChoiceC.addEventListener('click', function(event) {
+    event.stopPropagation();
+    correctAnswer = questionsArray[i].correctAnswer;
+    
+    if (questionsArray[i].correctAnswer === correctAnswer) { 
+        document.getElementById("AnswerResponse").innerHTML = "Correct! Nailed it!";
+        setTimeout(function() {
+            document.getElementById("AnswerResponse").innerHTML = "";
+                },
+                1000
+            );
+        score++;
+        document.getElementById("score").innerHTML = score;
+    } else {
+        time_remaining -= 5;
+        document.getElementById("AnswerResponse").innerHTML = "Incorrect! Better luck in the next one!";
+        setTimeout(function() {
+            document.getElementById("AnswerResponse").innerHTML = "";
+                },
+                1000
+            );
+    }
+    if (i >= questionsArray.length -1) {
+    end_quiz();
+    } else {
+        i++ 
+        setQuizQuestions();
+    };
+    });
+
+answerChoiceD.addEventListener('click', function(event) {
+    event.stopPropagation();
+    correctAnswer= questionsArray[i].correctAnswer;
+    
+    if (questionsArray[i].correctAnswer === correctAnswer) { 
+        document.getElementById("AnswerResponse").innerHTML = "Correct! Nailed it!";
+        setTimeout(function() {
+            document.getElementById("AnswerResponse").innerHTML = "";
+                },
+                1000
+            );
+        score++;
+        document.getElementById("score").innerHTML = score;
+    } else {
+        time_remaining -= 5;
+        document.getElementById("AnswerResponse").innerHTML = "Incorrect! Better luck in the next one!";
+        setTimeout(function() {
+            document.getElementById("AnswerResponse").innerHTML = "";
+                },
+                1000
+            );
+    }
+    if (i >= questionsArray.length -1) {
+       end_quiz();
+    } else {
+        i++ 
+        setQuizQuestions();
+    };
+});
+
+        //end quiz
+        function end_quiz(){
+            document.getElementById("game_over").style.display= "block";
+            document.getElementById("quizContainer").style.display="none";
+            document.getElementById("countdownTimer").style.display= "none";
+            document.getElementById("score_keeper").style.display= "none";
+            document.getElementById("AnswerResponse").innerHTML="";
+            document.getElementById("end_score").innerHTML= score;
+            }
+
+        
+            function submit_score() {
+             high_scores.push(document.getElementById("initials").value + " " + score);
+             view_high_scores();
+            }
+
+       
+        
+        function view_high_scores(){
+        
+       
+            document.getElementById("quizContainer").style.display="none";
+            document.getElementById("game_over").style.display= "none";
+            document.getElementById("high_scores_page").style.display="block";
+        
+            output="";
+            for(let k=0; k<high_scores.length; k++){
+                 output = output + "  " + high_scores[k];
+            }
+            document.getElementById("high_scores").innerHTML= output;                
+             clear_up();
+        }
+
+        
+        function go_home(){	
+                document.getElementById("high_scores_page").style.display= "none";
+                document.getElementById("homeContainer").style.display= "block";
+                clear_up();
+        }
+        
+        
+        function clear_hs(){
+            high_scores = [];
+            
+          }
+        
+        
+        function clear_up(){
+        
+        time=75;
+        time_remaining=true;
+        time_start=false;
+        i=0;
+        score=0;
+        }
---
+++ assets/js/questions.js
@@ -0,0 +1,36 @@
+// list of all questions, choices, and answers
+var  questionsArray = [
+  {
+    question: 'Commonly used data types DO NOT include:',
+    answerChoice: ['strings', 'booleans', 'alerts', 'numbers'],
+    correctAnswer: 'alerts',
+  },
+  {
+    question: 'The condition in an if / else statement is enclosed within ____.',
+    answerChoice: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+    correctAnswer: 'parentheses',
+  },
+  {
+    question: 'Arrays in JavaScript can be used to store ____.',
+    answerChoice: [
+      'numbers and strings',
+      'other arrays',
+      'booleans',
+      'all of the above',
+    ],
+    correctAnswer: 'all of the above',
+  },
+  {
+    question:
+      'String values must be enclosed within ____ when being assigned to variables.',
+      answerChoice: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+      correctAnswer: 'quotes',
+  },
+  {
+    question:
+      'A very useful tool used during development and debugging for printing content to the debugger is:',
+      answerChoice: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+      correctAnswer: 'console.log',
+  },
+];
+
